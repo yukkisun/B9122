6440b0f5219e09c714a93158ac04c328091557dc
yukkisun <yukkisun@163.com>
2019-10-11 11:49:57 -0400

    add a new file to the main repository

---
+++ question1.py
@@ -0,0 +1,18 @@
+## b9122 Assignment 1 ##
+# author: Yuqi(Yukki) Sun #
+# uni: ys3248 #
+# datetime: 20190925 #
+# email: YSun21@gsb.columbia.edu #
+
+## Question 1 ##
+# define the EMI function
+def EMI(rate, N, PV, FV):
+    if isinstance(rate, float) & isinstance(N, int) & isinstance(PV, int) & isinstance(FV, int):
+        emi = (PV + FV/(1 + rate)**N)*rate*(1 + rate)**N/((1 + rate)**N - 1)
+        print('EMI = ', emi)
+    else:
+        print('Please check your input. Reenter your input so that rate is float, N PV FV are all integer.')
+
+# test EMI function with given values
+EMI(0.05, 30, 10000, 2000)
+# also can use other inputs to test the EMI function: EMI(0.5,12.3,1000,20)
